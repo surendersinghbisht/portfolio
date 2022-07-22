bba89cab65a70d6f95b9fc6d15558b8d22bbcfc7
surender singh <singhsurender28231@gmail.com>
2022-07-22 20:41:16 +0530

    images

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -18,33 +18,33 @@ function Projects() {
         </P1>
 
         <ProjectsList
-          image="./public/todo.png"
+          image="./public/todo.jpg"
           details=" ToDo List App is a kind of app that generally used to maintain our
             day-to-day tasks or list everything that we have to do."
           href="https://todoappbysurender.netlify.app/"
-          heading='TODO APP'
+          heading="TODO APP"
         />
 
         <ProjectsList
-          image={require("./public/atlas.png")}
+          image="./public/atlas.jpg"
           details="World Atlas is an educational geography website . As the name would suggest, it offers perspectives on the world in the form of flags and details. I believe that it is an amazing planet that we share, and we are on a quest to prove it."
           href="https://worldatlasbysurender.netlify.app/countries"
-          heading='WORLD ATLAS'
+          heading="WORLD ATLAS"
         />
 
         <ProjectsList
-          image="./public/code.png"
+          image="./public/code.jpg"
           details="It is a clone of website app.codeYogi.io which i made by using my various skills."
           href=""
-          heading='CODEYOGI CLONE'
+          heading="CODEYOGI CLONE"
         />
 
         <ProjectsList
-          image="./public/jud.png"
+          image="./public/jud.jpg"
           details=" Do you need to ask a question? Are you looking for answers? Jud
               offers you a space to ask anything you want."
           href="https://Assignment-34-Create-Ask-Jud-clone-SurenderSINGH8.codeyogiteam.repl.co"
-          heading='ASK JUD'
+          heading="ASK JUD"
         />
       </div>
     </div>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,45 +1,66 @@
 import React from "react";
 import { ImCancelCircle } from "react-icons/im";
-import {HashLink as Link} from 'react-router-hash-link';
+import { HashLink as Link } from "react-router-hash-link";
 
 function Sidebar(props) {
   return (
-   <div className="shadow-2xl absolute w-full top-2   right-0 w-screen ">
-<div className="sm:hidden block  flex justify-end sticky">
-<aside  aria-label="Sidebar ">
-
-<div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800  ">
-<ul className="space-y-2">
-<ImCancelCircle className=" mt-2 font-semibold h-4  w-4 mr-2  sm:hidden block flex ml-80 mb-6 " onClick={props.onCancel}></ImCancelCircle>
-<li>
-<a  className="flex items-center  p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white hover:text-yellow-500 dark:hover:bg-gray-700">
-<Link to='#Home' smooth className=" font-bold ml-72">HOME</Link>
-</a>
-</li>
-<hr />
-<li>
-<a  className="flex items-center p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white  dark:hover:bg-gray-700">
-<Link to='#Aboutme' smooth  className="flex-1 text-right whitespace-nowrap hover:text-yellow-400 font-bold">ABOUT</Link>
-</a>
-</li>
-<hr />
-<li>
-<a  className="flex items-center p-2 text-base font-sans text-gray-700  rounded-lg dark:text-white  dark:hover:bg-gray-700">
-<Link to='#Projects' smooth className="flex-1 text-right  whitespace-nowrap hover:text-yellow-400 font-bold">PROJECTS</Link>
-</a>
-</li>
-<hr />
-<li>
-<a  className="flex items-center p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white  dark:hover:bg-gray-700">
-<Link to='#Social' smooth className="flex-1 text-right whitespace-nowrap hover:text-yellow-400 font-bold font-bold">CONTACT</Link>
-</a>
-</li>
-
-</ul>
-</div>
-</aside>
-</div>
-</div>
+    <div className="shadow-2xl absolute w-full top-2   right-0 n ">
+      <div className="sm:hidden   flex justify-end sticky">
+        <aside aria-label="Sidebar ">
+          <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800  ">
+            <ul className="space-y-2">
+              <ImCancelCircle
+                className=" mt-2 font-semibold h-4  w-4 mr-2  sm:hidden  flex ml-80 mb-6 "
+                onClick={props.onCancel}
+              ></ImCancelCircle>
+              <li>
+                <a className="flex items-center  p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white hover:text-yellow-500 dark:hover:bg-gray-700">
+                  <Link to="#Home" smooth className=" font-bold ml-72">
+                    HOME
+                  </Link>
+                </a>
+              </li>
+              <hr />
+              <li>
+                <a className="flex items-center p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white  dark:hover:bg-gray-700">
+                  <Link
+                    to="#Aboutme"
+                    smooth
+                    className="flex-1 text-right whitespace-nowrap hover:text-yellow-400 font-bold"
+                  >
+                    ABOUT
+                  </Link>
+                </a>
+              </li>
+              <hr />
+              <li>
+                <a className="flex items-center p-2 text-base font-sans text-gray-700  rounded-lg dark:text-white  dark:hover:bg-gray-700">
+                  <Link
+                    to="#Projects"
+                    smooth
+                    className="flex-1 text-right  whitespace-nowrap hover:text-yellow-400 font-bold"
+                  >
+                    PROJECTS
+                  </Link>
+                </a>
+              </li>
+              <hr />
+              <li>
+                <a className="flex items-center p-2 text-base font-sans text-gray-700 rounded-lg dark:text-white  dark:hover:bg-gray-700">
+                  <Link
+                    to="#Social"
+                    smooth
+                    className="flex-1 text-right whitespace-nowrap hover:text-yellow-400 font-bold "
+                  >
+                    CONTACT
+                  </Link>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </div>
+    </div>
   );
 }
 
